39825172b5177da9ff6b240a9de9766d515bb4f9
hsjoihs <hs.ioling.hs@gmail.com>
2017-10-29 08:59:11 +0900

    Write

---
+++ drawTable.js
@@ -0,0 +1,25 @@
+function drawTable(arr)
+{
+	var str='<table border="1">';
+	for(var i=-1;i<arr.length;i++){
+		str+='<tr>';
+		for(var j=-1;j<arr.length;j++){
+
+			if(i==-1){
+				if(j!=-1){str+='<td>'+arr[j][0]+'</td>';}else{
+					str+='<td>'+'</td>';
+				}
+			} else {
+				if(j==-1){str+='<td>'+arr[i][0]+'</td>';} else{
+					var val = arr[i][1]/arr[j][1];
+					str+= '<td class="s' + (isHarmonic(val) ? 2:1) + '">';
+					str+= val.toPrecision(5) +'</td>';
+				}
+			}
+
+		}
+		str+='</tr>';
+	}
+	str+="</table>"
+	document.write(str);
+}
--- isHarmonic.js
+++ isHarmonic.js
@@ -1,4 +1,23 @@
 function isHarmonic(value)
 {
-	
+	if(value > 2) {
+		return isHarmonic(value/2);
+	}
+	if(value < 1) {
+		return isHarmonic(value*2);
+	}
+	// within 0.95%
+	var arr = [1,2,3,4,5,6,8,9];
+	var arr2 = [1,2,3,4,5,6,8,9,10,12,16];
+	for(var q=0; q<arr.length; q++){
+		var i = arr[q];
+		for(var r=0; r<arr2.length; r++) {
+			var j=arr2[r];
+			var k = value/(j/i);
+			if(1 - 0.0095 <= k && k <= 1 + 0.0095) {
+				return [true,j,i];
+			}
+		}
+	}
+	return false;
 }
